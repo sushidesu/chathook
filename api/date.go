31b8e15eb23cffa9ea35d3e3eb7a495168269e26
sushidesu <hugemerge@gmail.com>
2021-06-03 05:14:12 +0000

    envのテスト

--- api/date.go
+++ api/date.go
@@ -3,10 +3,11 @@ package api
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	fmt.Fprintf(w, currentTime+", "+os.Getenv("VERCEL_ENV"))
 }
