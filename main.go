410ad1bdff5e8690a47ccd1092a1098823e1c7fd
sushidesu <hugemerge@gmail.com>
2021-06-17 06:08:25 +0000

    fix: サンプルコードにパースのユースケースを追加

--- main.go
+++ main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/sushidesu/chathook/infra/airtable"
+	parsemessage "github.com/sushidesu/chathook/usecase/parse_message"
 	recordattendance "github.com/sushidesu/chathook/usecase/record_attendance"
 )
 
 func main() {
 	godotenv.Load()
 
+	// エアテーブル記録サンプル
 	airtable := airtable.Airtable{
 		BaseUrl: os.Getenv("AIRTABLE_BASE_URL"),
 		ApiKey:  os.Getenv("AIRTABLE_API_KEY"),
 	}
-
 	recordAttendanceUsecase := recordattendance.NewRecordAttendanceUsecase(airtable)
 	recordAttendanceUsecase.Record("LEAVE", "OFFICE")
+
+	// メッセージパースサンプル
+	parseMessageUsecase := parsemessage.ParseMessageUsecase{}
+	result := parseMessageUsecase.Parse("こんにちは、お疲れ様です(bow)")
+	fmt.Println(result.Type.Value)
 }
