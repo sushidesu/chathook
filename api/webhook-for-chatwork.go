45690d5f30f3de7044c59992715be6ef913deb81
sushidesu <hugemerge@gmail.com>
2021-06-17 03:02:30 +0000

    feat: 署名を検証する部分まで実装

---
+++ api/webhook-for-chatwork.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sushidesu/chathook/infra/chatwork"
+)
+
+func WebhookForChatwork(w http.ResponseWriter, r *http.Request) {
+	godotenv.Load()
+
+	// ヘッダ、ボディを取得
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	signature := r.Header.Get("X-ChatWorkWebhookSignature")
+
+	// 署名の検証
+	validator := chatwork.RequestValidator{WebhookToken: os.Getenv("CHATWORK_WEBHOOK_TOKEN")}
+	valid, err := validator.Validate(body, signature)
+	if err != nil || !valid {
+		return
+	}
+
+	// ボディをパース
+	// Airtableへ結果を保存
+}
